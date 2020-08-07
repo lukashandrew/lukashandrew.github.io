2e65713096d48382520dd64aa3fc7a0fdab30b78
lukashandrew <68441445+lukashandrew@users.noreply.github.com>
2020-08-07 13:16:18 +0300

    Создал сайт прорыв
    добавил файлы и папки

---
+++ Proriv/js/script.js
@@ -0,0 +1,13 @@
+
+	$(document).ready(function () {
+
+		$('.panel-heading').click(function () {
+			$(this).toggleClass('in').next().slideToggle();
+			$('.panel-heading').not(this).removeClass('in').next().slideUp();
+        });
+
+    });	
+
+
+
+
